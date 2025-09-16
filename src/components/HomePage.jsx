50e5f6ac5d8db1b9695d29272c302c72fb569fc4
akshaychandel1 <chandel2102@gmail.com>
2025-09-17 00:43:43 +0530

    das

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -400,7 +400,7 @@ const HomePage = () => {
         <h2 className="text-3xl sm:text-4xl md:text-6xl 2xl:text-8xl font-barber leading-tight md:leading-wide text-center md:text-left">
           WEDDING & EVENTS
         </h2>
-        <p className="mt-2 text-sm sm:text-base md:text-sm 2xl:text-lg leading-relaxed text-center md:text-left">
+        <p className="mt-2 text-sm sm:text-base md:text-sm 2xl:text-xl leading-relaxed text-center md:text-left">
           From grand weddings to lively celebrations, Saada Halwai serves
           the soul of India with every plate. Think sizzling street-style
           chaats, rich royal curries, and decadent mithai, all crafted
@@ -444,7 +444,7 @@ const HomePage = () => {
         <h2 className="text-3xl sm:text-4xl md:text-7xl 2xl:text-8xl font-barber leading-tight md:leading-wide">
           Corporate Events
         </h2>
-        <p className="mt-2 text-sm sm:text-base md:text-sm 2xl:text-lg leading-relaxed">
+        <p className="mt-2 text-sm sm:text-base md:text-sm 2xl:text-xl leading-relaxed">
           Make your corporate events unforgettable with Saada Halwai. From
           vibrant chaats to rich curries and indulgent mithai, we serve
           authentic Indian flavours with a polished touch. Freshly cooked
@@ -488,7 +488,7 @@ const HomePage = () => {
         <h2 className="text-3xl sm:text-4xl md:text-6xl 2xl:text-8xl font-barber leading-tight md:leading-wide">
           Grab & GO
         </h2>
-        <p className="mt-2 text-sm sm:text-base md:text-sm 2xl:text-lg leading-relaxed">
+        <p className="mt-2 text-sm sm:text-base md:text-sm 2xl:text-xl leading-relaxed">
           Perfect for busy days and hungry moments, Saada Halwai’s Grab & Go
           brings Indian flavours to your hands in minutes. From spicy chaats
           to hearty curries and sweet mithai, every bite is fresh, authentic,
